cd0c9fc88cae549272bd139546bd50449be12e16
Andrea Ascari <andrea.ascari@buildo.io>
2017-12-20 13:03:48 +0100

    Created methods to save and retrieve data from localStorage

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,7 +1,25 @@
 const store = {
-  getMessages: () => {
-    return window.localStorage.getItem('messages') || []
+  getGuestUsername() {
+   return window.localStorage && window.localStorage.getItem('guestUsername');
+  },
+  setUsername(username) {
+    window.localStorage.setItem('username', username);
+  },
+  getUsername() {
+    return window.localStorage && window.localStorage.getItem('username');
+  },
+  getMessages() {
+    return window.localStorage.getItem('messages') ? JSON.parse(window.localStorage.getItem('messages')) : []
+  },
+  addMessage(message) {
+
+    const storedMessages = window.localStorage.getItem('messages') ? JSON.parse(window.localStorage.getItem('messages')) : [];
+    const messages = storedMessages.concat(message);
+    window.localStorage.setItem('messages', JSON.stringify(messages));
   }
 }
 
 export { store };
+
+export const randomUsername = () =>
+  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);

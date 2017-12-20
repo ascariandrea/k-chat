7f2fd97379b76d31a9cb7d512e09fd9636e04072
Andrea Ascari <andrea.ascari@buildo.io>
2017-12-20 10:41:54 +0100

    Created webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: ['babel-loader']
+    },{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }]
+  },
+  devServer: {
+    contentBase: './build'
+  }
+};

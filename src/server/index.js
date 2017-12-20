1bcc498591c2b98a22a29fb36a91eb83acb8b6e0
Andrea Ascari <andrea.ascari@buildo.io>
2017-12-20 13:03:21 +0100

    Dispatch events to all client connected

--- src/server/index.js
+++ src/server/index.js
@@ -8,12 +8,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let sockets = [];
 io.on("connection", (socket) => {
-  console.log("New client connected"));
+  console.log("New client connected");
 
-  socket.on("new-message", (data) => {
-    socket.emit('new-message-received', data);
-  })
+  socket.on('new-message', data => io.emit('new-message', data));
+  socket.on('username-updated', data => io.emit('username-updated', data));
 
   socket.on("disconnect", () => console.log("Client disconnected"));
 });
